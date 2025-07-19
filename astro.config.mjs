e903f9febe883832ef995c1b9a87e83f0075f0ce
tnGJaTKZQC <dAn0EcugSQ@tutamail.com>
2025-07-18 19:23:07 -0700

    sdsa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-// import vercel from "@astrojs/vercel/serverless"; // o /edge, /static
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless"; // o /edge, /static
 
 export default defineConfig({
   output: "server", // o 'static' o 'hybrid'
-  adapter: node({ mode: "standalone" }),
-  //   adapter: vercel({
-  //     isr: false,
-  //     webAnalytics: { enabled: true },
-  //     // otras opciones...
-  //   }),
+  //   adapter: node({ mode: "standalone" }),
+  adapter: vercel({
+    isr: false,
+    webAnalytics: { enabled: true },
+    // otras opciones...
+  }),
 });
 // npm install @astrojs/vercel
 // npm install @astrojs/node
