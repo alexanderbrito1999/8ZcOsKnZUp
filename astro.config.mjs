4f05ed08ee131df706ec656fe80a6bb5bed05e45
tnGJaTKZQC <dAn0EcugSQ@tutamail.com>
2025-07-18 19:18:38 -0700

    sdas

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+// import vercel from "@astrojs/vercel/serverless"; // o /edge, /static
+
+export default defineConfig({
+  output: "server", // o 'static' o 'hybrid'
+  adapter: node({ mode: "standalone" }),
+  //   adapter: vercel({
+  //     isr: false,
+  //     webAnalytics: { enabled: true },
+  //     // otras opciones...
+  //   }),
+});
+// npm install @astrojs/vercel
+// npm install @astrojs/node
+
+// npm install @astrojs/netlify
+// import { defineConfig } from 'astro/config';
+// import netlify from '@astrojs/netlify';
+
+// export default defineConfig({
+//   output: 'server', // <--- SSR dinámico
+//   adapter: netlify(),
+// });
+
+// npm run build
+// node dist/server/entry.mjs
